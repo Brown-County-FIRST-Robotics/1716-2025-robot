c929e8fc3c1a0cd3975f479ae6d8d2262e06987a
gldeA <glde.abbott@gmail.com>
2025-02-11 09:57:15 -0600

    Updated Manipulator BB Module to match with new control layout

--- src/main/java/frc/robot/utils/buttonbox/ManipulatorPanel.java
+++ src/main/java/frc/robot/utils/buttonbox/ManipulatorPanel.java
@@ -3,62 +3,66 @@ package frc.robot.utils.buttonbox;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class ManipulatorPanel extends ButtonBoxPanel {
-  private boolean doAlgae;
+  //Digital Input IDs, assuming that level2 is TROUGH_ID + 1, etc.
+  private static final TROUGH_ID = 0;
+  private static final LEFT_POLE_ID = 4;
+  private static final EJECT_ID = 6;
 
   public ManipulatorPanel(ButtonBox bb) {
     super(bb);
-    updateAlgaeSwitch();
   }
 
-  public Trigger leftPole() {
-    return new Trigger(() -> getButton(0));
-  }
-
-  public Trigger rightPole() {
-    return new Trigger(() -> getButton(1));
-  }
-
-  public Trigger noPole() { // checks if both or neither pole buttons are pressed
-    return new Trigger(
-        () ->
-            (leftPole().getAsBoolean() && rightPole().getAsBoolean())
-                || (!leftPole().getAsBoolean() && !rightPole().getAsBoolean()));
-  }
-
-  public Trigger trough() {
-    updateAlgaeSwitch();
-    return new Trigger(() -> !doAlgae && getButton(2));
+  public Trigger trough() { //level 1 coral
+    return new Trigger(() -> getButton(TROUGH_ID) && !getButton(TROUGH_ID + 1)); //only activate if algaeLow won't activate
   }
 
   public Trigger level2() {
-    updateAlgaeSwitch();
-    return new Trigger(() -> !doAlgae && getButton(3));
+    return new Trigger(() -> getButton(TROUGH_ID + 1) && !getButton(TROUGH_ID));
   }
 
   public Trigger level3() {
-    updateAlgaeSwitch();
-    return new Trigger(() -> !doAlgae && getButton(4));
+    return new Trigger(() -> getButton(TROUGH_ID + 2) && !getButton(TROUGH_ID + 3));
   }
 
   public Trigger level4() {
-    updateAlgaeSwitch();
-    return new Trigger(() -> !doAlgae && getButton(5));
+    return new Trigger(() -> getButton(TROUGH_ID + 3) && !getButton(TROUGH_ID + 2));
   }
 
   public Trigger algaeLow() {
-    updateAlgaeSwitch();
-    // bottom two levels work for low algae
-    return new Trigger(() -> doAlgae && (trough().getAsBoolean() || level2().getAsBoolean()));
+    //press both bottom buttons for low algae
+    return new Trigger(() -> getButton(TROUGH_ID) && getButton(TROUGH_ID + 1));
   }
 
   public Trigger algaeHigh() {
-    updateAlgaeSwitch();
-    // top two levels work for high algae
-    return new Trigger(() -> doAlgae && (level3().getAsBoolean() || level4().getAsBoolean()));
+    //press both top buttons for high algae
+    return new Trigger(() -> getButton(TROUGH_ID + 2) && getButton(TROUGH_ID + 3));
   }
 
-  private void updateAlgaeSwitch() {
-    doAlgae = getButton(7);
+  public Trigger leftPole() {
+    return new Trigger(() -> getButton(LEFT_POLE_ID) && !getButton(LEFT_POLE_ID + 1)); //only activate if not both are pressed
+  }
+
+  public Trigger rightPole() {
+    return new Trigger(() -> getButton(LEFT_POLE_ID + 1) && !getButton(LEFT_POLE_ID));
+  }
+
+  // checks if neither pole buttons are pressed
+  public Trigger noPole() {
+    return new Trigger(
+        () -> !getButton(LEFT_POLE_ID) && !getButton(LEFT_POLE_ID + 1));
+  }
+
+  public Trigger processor() {
+    //TODO: check whether the robot has an algae or not to determine whether to do processor or intake
+    return new Trigger(() -> getButton(LEFT_POLE_ID) && getButton(LEFT_POLE_ID + 1));
+  }
+
+  public Trigger intake() { //coral station
+    return new Trigger(() -> getButton(LEFT_POLE_ID) && getButton(LEFT_POLE_ID + 1));
+  }
+  
+  public Trigger eject() {
+    return new Trigger(() -> getButton(EJECT_ID));
   }
 
   @Override
