1c241552a64cc66c91d69ef50f6333d9732e5f18
cfinn <cfinn68916@gmail.com>
2025-03-10 15:39:05 -0500

    Unstaged changes during deploy at 20250310_153905

--- src/main/java/frc/robot/subsystems/vision/VisionSLAMIOculus.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSLAMIOculus.java
@@ -1,4 +1,43 @@
 package frc.robot.subsystems.vision;
 
-public class VisionSLAMIOculus {
+import edu.wpi.first.math.geometry.Quaternion;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.*;
+
+public class VisionSLAMIOculus implements VisionSLAMIO {
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("questnav");
+  DoubleArraySubscriber questRot =
+      table.getDoubleArrayTopic("quaternion").subscribe(new double[] {});
+  DoubleArraySubscriber questPosition =
+      table.getDoubleArrayTopic("position").subscribe(new double[] {});
+  IntegerSubscriber questFrames = table.getIntegerTopic("frameCount").subscribe(0);
+
+  @Override
+  public void updateInputs(VisionSLAMIOInputs inputs) {
+    if (questRot.get().length > 0 && questPosition.get().length > 0) {
+      inputs.present = true;
+      inputs.questTranslation =
+          new Translation3d(questPosition.get()[0], questPosition.get()[1], questPosition.get()[2]);
+      inputs.questQuat =
+          new Quaternion(
+              questRot.get()[0], questRot.get()[1], questRot.get()[2], questRot.get()[3]);
+      inputs.frames = questFrames.get();
+    } else {
+      inputs.present = false;
+    }
+  }
+
+  @Override
+  public void ADBKill() {
+    VisionSLAMIO.super.ADBKill();
+  }
+
+  @Override
+  public void ADBStart() {
+    try {
+      Runtime.getRuntime().exec(new String[] {"asd", "asdsa"});
+    } catch (Exception e) {
+
+    }
+  }
 }
