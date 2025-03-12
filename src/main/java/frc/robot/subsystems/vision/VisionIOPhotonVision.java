e7ec10fe0b757d640c8170fe94fcee3835d31fc7
cfinn <cfinn68916@gmail.com>
2025-03-12 17:07:20 -0500

    Unstaged changes during deploy at 20250312_170720

--- src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVision.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOPhotonVision.java
@@ -2,9 +2,7 @@ package frc.robot.subsystems.vision;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.*;
 import java.util.Optional;
 import org.photonvision.PhotonCamera;
@@ -12,26 +10,24 @@ import org.photonvision.PhotonPoseEstimator;
 
 /** The IO layer for one SecondSight camera */
 public class VisionIOPhotonVision implements VisionIO {
-  final PhotonCamera cam = new PhotonCamera("Arducam_OV2311_USB_Camera (1)");
-  final Transform3d robotToCam =
-      new Transform3d(
-          new Translation3d(2 * 0.0254, 0 * 0.0254, 24 * 0.0254),
-          new Rotation3d(0, -25.0 * Math.PI / 180, -4.6 * Math.PI / 180));
+  final PhotonCamera cam;
 
   // Construct PhotonPoseEstimator
-  final PhotonPoseEstimator photonPoseEstimator =
-      new PhotonPoseEstimator(
-          AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape), // TODO: Fix
-          PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
-          robotToCam);
-
+  final PhotonPoseEstimator photonPoseEstimator;
   /**
    * Constructs a new <code>VisionIOPhotonVision</code> from a NT path
    *
    * @param inst_name The NT path of the instance
    * @param cam_name The name of the camera
    */
-  public VisionIOPhotonVision(String inst_name, String cam_name) {}
+  public VisionIOPhotonVision(String cam_name, Transform3d camPose) {
+    cam = new PhotonCamera(cam_name);
+    photonPoseEstimator =
+        new PhotonPoseEstimator(
+            AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape), // TODO: Fix
+            PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+            camPose);
+  }
 
   @Override
   public void updateInputs(VisionIOInputs inputs) {
--- src/main/java/frc/robot/utils/PoseEstimator.java
+++ src/main/java/frc/robot/utils/PoseEstimator.java
@@ -1,24 +1,49 @@
 package frc.robot.utils;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import frc.robot.subsystems.vision.Vision;
-import java.util.*;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.subsystems.vision.FusedVision;
+import java.util.Optional;
 
 /** A pose estimator that fuses vision and odometry updates */
 public class PoseEstimator {
   // Ship of Theseused from
-  public Vision pt;
+  public Optional<FusedVision> pt = Optional.empty();
   // https://github.com/wpilibsuite/allwpilib/blob/1db3936965bd8ed33224ad388cf9f16d12999a08/wpimath/src/main/java/edu/wpi/first/math/estimator/PoseEstimator.java
   Pose2d current = Pose2d.kZero;
+  boolean usedVis = false;
 
   public Pose2d getPose() {
-    return pt.getPose();
+    if (pt.isPresent()) return pt.get().getSlamPose();
+    else return Pose2d.kZero;
   }
 
   public void setPose(Pose2d pz) {
-    pt.setpos(pz);
+    pt.ifPresent(fusedVision -> fusedVision.setpos(pz));
+  }
+
+  public void feed() {
+    if (new XboxController(0).getAButtonPressed()) {
+      usedVis = false;
+    }
+    if (pt.isPresent()) {
+      if (DriverStation.isDisabled() && !usedVis && pt.get().isActive()) {
+        if (pt.get().inputs.pose.isPresent()) {
+          pt.get().setpos(pt.get().inputs.pose.get().toPose2d());
+          usedVis = true;
+        }
+      }
+    }
   }
 
   /** Constructs a new pose estimator */
-  public PoseEstimator() {}
+  public PoseEstimator() {
+    new PeriodicRunnable() {
+      @Override
+      public void periodic() {
+        feed();
+      }
+    };
+  }
 }
