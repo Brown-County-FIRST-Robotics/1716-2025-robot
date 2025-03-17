549615617eaf2c7a619fe8178b7a748ebab9725a
Laptop13 <test@example.com>
2025-03-17 17:52:32 -0500

    Unstaged changes during deploy at 20250317_175231

--- src/main/java/frc/robot/subsystems/manipulator/WristIOSparkFlex.java
+++ src/main/java/frc/robot/subsystems/manipulator/WristIOSparkFlex.java
@@ -15,7 +15,7 @@ import frc.robot.Constants;
 public class WristIOSparkFlex implements WristIO {
   private final SparkFlex wrist;
   private final AbsoluteEncoder encoder;
-  private final double offset = 0.5043; // TESTME
+  private final double offset = 0.6;
 
   public WristIOSparkFlex(int id) {
     wrist = new SparkFlex(id, MotorType.kBrushless);
@@ -28,14 +28,14 @@ public class WristIOSparkFlex implements WristIO {
         .smartMotion
         .maxAcceleration(2.0 * scaling / 6700.0)
         .maxVelocity(0.16 * scaling / 6700.0); // TESTME
-    wristConfig.smartCurrentLimit(Constants.CurrentLimits.NEO_VORTEX).idleMode(IdleMode.kBrake);
+    wristConfig.smartCurrentLimit(Constants.CurrentLimits.NEO_VORTEX).idleMode(IdleMode.kCoast);
     wristConfig.closedLoop.feedbackSensor(FeedbackSensor.kAbsoluteEncoder);
 
     wrist.configure(wristConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   public void updateInputs(WristIOInputs inputs) {
-    inputs.angle = encoder.getPosition() - offset;
+    inputs.angle = (encoder.getPosition() + offset) - Math.floor(encoder.getPosition() + offset);
     inputs.omega = encoder.getVelocity();
 
     inputs.appliedOutput = wrist.getAppliedOutput();
