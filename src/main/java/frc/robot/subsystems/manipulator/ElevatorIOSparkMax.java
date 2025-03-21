6379540be69d33bee04131e38ec7e40396007468
Laptop13 <test@example.com>
2025-03-21 18:02:53 -0500

    Unstaged changes during deploy at 20250321_180252

--- src/main/java/frc/robot/subsystems/manipulator/ElevatorIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/manipulator/ElevatorIOSparkMax.java
@@ -27,7 +27,7 @@ public class ElevatorIOSparkMax implements ElevatorIO {
         .closedLoop
         .smartMotion
         .maxAcceleration(8000)
-        .maxVelocity(6000)
+        .maxVelocity(300)
         .minOutputVelocity(0); // placeholder
     elevatorConfig
         .closedLoop
