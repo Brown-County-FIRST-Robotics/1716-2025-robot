4e3d1cd32228817bf28fb0c22407e619f87479bd
cfinn <cfinn68916@gmail.com>
2025-06-08 12:09:57 -0500

    No IMU :'(
    Signed-off-by: cfinn <cfinn68916@gmail.com>

--- src/main/java/frc/robot/subsystems/swerve/SwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveDrivetrain.java
@@ -6,9 +6,7 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.math.kinematics.*;
 import edu.wpi.first.math.numbers.N3;
-import frc.robot.Constants;
 import frc.robot.subsystems.*;
-import frc.robot.utils.Overrides;
 import frc.robot.utils.PoseEstimator;
 import org.littletonrobotics.junction.Logger;
 
@@ -27,22 +25,7 @@ public class SwerveDrivetrain implements Drivetrain {
   final Module bl;
   final Module br;
 
-  Rotation3d lastIMU;
-  SwerveModulePosition[] lastPositions;
   final PoseEstimator poseEstimator;
-
-  final IMUIO imu;
-  final IMUIOInputsAutoLogged imuInputs = new IMUIOInputsAutoLogged();
-
-  private SwerveModulePosition[] getPositions() {
-
-    return new SwerveModulePosition[] {
-      fl.getChassisRelativePosition(),
-      fr.getChassisRelativePosition(),
-      bl.getChassisRelativePosition(),
-      br.getChassisRelativePosition()
-    };
-  }
 
   /**
    * Creates a SwerveDrivetrain from IO
@@ -51,47 +34,25 @@ public class SwerveDrivetrain implements Drivetrain {
    * @param fr Front right module IO
    * @param bl Back left module IO
    * @param br Back right module IO
-   * @param imu IMU IO
    */
-  public SwerveDrivetrain(Module fl, Module fr, Module bl, Module br, IMUIO imu) {
-    this.imu = imu;
+  public SwerveDrivetrain(Module fl, Module fr, Module bl, Module br) {
     this.fl = fl;
     this.fr = fr;
     this.bl = bl;
     this.br = br;
     poseEstimator = new PoseEstimator();
     //    poseEstimator.setPose(Constants.INIT_POSE);
-    lastIMU = getGyro();
-    lastPositions = getPositions();
   }
 
   @Override
   public void periodic() {
-    imu.updateInputs(imuInputs);
-    Logger.processInputs("Drive/IMU", imuInputs);
     fl.periodic();
     fr.periodic();
     bl.periodic();
     br.periodic();
 
     Logger.recordOutput("Drive/RealStates", getWheelSpeeds());
-    Twist2d odoTwist = KINEMATICS.toTwist2d(lastPositions, getPositions());
-    if (!Overrides.disableIMU.get()) {
-      getGyro().minus(lastIMU).getX();
-    }
-    lastPositions = getPositions();
-    lastIMU = getGyro();
     Logger.recordOutput("Drive/Pose", getPosition());
-
-    checkForYawReset();
-  }
-
-  private void checkForYawReset() {
-    if (Overrides.resetYaw.get()) {
-      poseEstimator.setPose(
-          new Pose2d(getPosition().getTranslation(), Constants.INIT_POSE.getRotation()));
-      Overrides.resetYaw.set(false);
-    }
   }
 
   private SwerveModuleState[] getWheelSpeeds() {
@@ -129,16 +90,6 @@ public class SwerveDrivetrain implements Drivetrain {
   public void humanDrive(ChassisSpeeds cmd) {
     SwerveModuleState[] states = KINEMATICS.toSwerveModuleStates(cmd);
     setModuleStates(states);
-  }
-
-  @Override
-  public Rotation3d getGyro() {
-    return imuInputs.rotation;
-  }
-
-  @Override
-  public double[] getAcceleration() {
-    return new double[] {imuInputs.xAccelMPS, imuInputs.yAccelMPS, imuInputs.zAccelMPS};
   }
 
   @Override
