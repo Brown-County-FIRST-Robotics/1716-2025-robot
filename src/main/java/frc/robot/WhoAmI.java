5d888a651bf38b901639db47d8a536c3fdb7956c
cfinn <cfinn68916@gmail.com>
2025-06-08 15:28:39 -0500

    Unstaged changes during deploy at 20250608_152839

--- src/main/java/frc/robot/WhoAmI.java
+++ src/main/java/frc/robot/WhoAmI.java
@@ -10,7 +10,7 @@ public final class WhoAmI {
   public static final RobotType bot = RobotType.SWERVEBASE;
   /** The appendages to the robot */
   public static final Appendages[] appendages = {
-    Appendages.ELEVATOR, Appendages.WRIST, Appendages.GRIPPER // , Appendages.CLIMBER
+    // Appendages.ELEVATOR, Appendages.WRIST, Appendages.GRIPPER // , Appendages.CLIMBER
   };
 
   public static final boolean isDemoMode = false;
