d8619911490747c93600598bd4e3c106cb4427e5
cfinn <cfinn68916@gmail.com>
2025-06-24 19:38:08 -0500

    Unstaged changes during deploy at 20250624_193808

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -108,7 +108,7 @@ public class RobotContainer {
           gripperIO = new GripperIOSparkMax(3, 1, 0);
         }
         if (appendage == WhoAmI.Appendages.ELEVATOR) {
-          elevatorIO = new ElevatorIOSparkMax(53, -10000);
+          elevatorIO = new ElevatorIOSparkMax(53, 54, 2);
         }
         if (appendage == WhoAmI.Appendages.CLIMBER) {
           climberIO = new ClimberIOSparkMaxes(54, 2);
@@ -370,18 +370,29 @@ public class RobotContainer {
             Commands.runEnd(() -> gripper.setGripper(-3000), () -> gripper.setGripper(0), gripper));
 
     driverController.back().onTrue(Commands.runOnce(() -> driveSys.setPosition(Pose2d.kZero)));
+    driverController
+        .povUp()
+        .whileTrue(
+            Commands.run(() -> manipulator.setElevatorReference(manipulator.getElevator() + 0.1)));
+    driverController
+        .povDown()
+        .whileTrue(
+            Commands.run(() -> manipulator.setElevatorReference(manipulator.getElevator() - 0.1)));
+
+    // TEMP CODE
+    driverController.a().whileTrue(presetFactory.intake());
 
     // Climber
-    driverController
-        .a()
-        .or(driverController.povDown())
-        .or(driverController.povDownLeft())
-        .or(driverController.povDownRight())
-        .onTrue(
-            Commands.runOnce(() -> climber.setServo(true), climber)
-                .andThen(
-                    Commands.waitSeconds(.5)
-                        .andThen(Commands.run(() -> climber.setPosition(true), climber))));
+    //    driverController
+    //        .a()
+    //        .or(driverController.povDown())
+    //        .or(driverController.povDownLeft())
+    //        .or(driverController.povDownRight())
+    //        .onTrue(
+    //            Commands.runOnce(() -> climber.setServo(true), climber)
+    //                .andThen(
+    //                    Commands.waitSeconds(.5)
+    //                        .andThen(Commands.run(() -> climber.setPosition(true), climber))));
     driverController
         .y()
         .or(driverController.povUp())
--- src/main/java/frc/robot/subsystems/manipulator/ElevatorIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/manipulator/ElevatorIOSparkMax.java
@@ -10,15 +10,20 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class ElevatorIOSparkMax implements ElevatorIO {
   private final SparkFlex elevator_primary;
   private final SparkFlex elevator_follower;
-  private final RelativeEncoder elevatorEncoder;
 
-  public ElevatorIOSparkMax(int id, int follower_id) {
+  public static final double scle = 0.0254 * 5.5 * 2.0 / 15.0; // m/Rotations // travel=29*0.0254
+  private final RelativeEncoder elevatorEncoder;
+  private final DigitalInput limitSwitch;
+
+  public ElevatorIOSparkMax(int id, int follower_id, int limit_switch_port) {
     elevator_primary = new SparkFlex(id, MotorType.kBrushless);
     elevator_follower = new SparkFlex(follower_id, MotorType.kBrushless);
+    limitSwitch = new DigitalInput(limit_switch_port);
     SparkMaxConfig elevatorConfig = new SparkMaxConfig();
     elevatorEncoder = elevator_primary.getEncoder();
 
@@ -38,33 +43,32 @@ public class ElevatorIOSparkMax implements ElevatorIO {
     elevatorConfig
         .smartCurrentLimit(
             60) // Lesser current limit to prevent elevator mechanically falling apart
-        .inverted(false)
+        .inverted(true)
         .idleMode(IdleMode.kBrake);
     elevator_primary.configure(
         elevatorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     SparkMaxConfig follower = new SparkMaxConfig();
-    follower.follow(elevator_primary);
-    follower
-        .smartCurrentLimit(60)
-        .inverted(!elevator_primary.configAccessor.getInverted())
-        .idleMode(IdleMode.kBrake);
+    follower.follow(elevator_primary, true);
+    follower.smartCurrentLimit(60).idleMode(IdleMode.kBrake);
     elevator_follower.configure(
         follower, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    elevatorEncoder.setPosition(0.0);
   }
 
   public void updateInputs(ElevatorIOInputs inputs) {
-    inputs.position = elevatorEncoder.getPosition();
-    inputs.velocity = elevatorEncoder.getVelocity();
+    inputs.position = elevatorEncoder.getPosition() * scle;
+    inputs.velocity = elevatorEncoder.getVelocity() * scle / 60.0;
 
     inputs.appliedOutput = elevator_primary.getAppliedOutput();
     inputs.temperature_1 = elevator_primary.getMotorTemperature();
     inputs.temperature_2 = elevator_follower.getMotorTemperature();
     inputs.current = elevator_primary.getOutputCurrent();
+    inputs.limitSwitch = !limitSwitch.get();
   }
 
   public void setPosition(double commandPosition, double arbFF) {
     elevator_primary
         .getClosedLoopController()
-        .setReference(commandPosition, ControlType.kSmartMotion, ClosedLoopSlot.kSlot0, 0.2);
+        .setReference(commandPosition / scle, ControlType.kSmartMotion, ClosedLoopSlot.kSlot0, 0.2);
   }
 }
